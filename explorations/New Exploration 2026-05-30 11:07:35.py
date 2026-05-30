95922b1da75959c4623c19f400d90500033dd44b
jainvineet27 <vineetanything@gmail.com>
2026-05-30 18:07:04 +0000

    feat : push file to remote

--- explorations/New Exploration 2026-05-30 11:07:35.py
+++ explorations/New Exploration 2026-05-30 11:07:35.py
@@ -1,4 +1,14 @@
 # Databricks notebook source
+# MAGIC %sql
+# MAGIC select * from namaste_catalog.lakeflowconnect.orders_silver
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC select  * from  namaste_catalog.lakeflowconnect.orders_silver
+
+# COMMAND ----------
+
 df =spark.read.format('csv').option('header',True).load("/Volumes/namaste_catalog/vineetdb/testvolume/source_files/orders_files/customers-100.csv")
 display(df)
 
